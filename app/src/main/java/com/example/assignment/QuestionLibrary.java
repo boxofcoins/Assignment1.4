0d56afcc2646921553ab6e1b408514ad0748238c
eddie <eddiehu1@hotmail.com>
2018-10-25 18:13:02 +1100

    hi

--- app/src/main/java/com/example/assignment/QuestionLibrary.java
+++ app/src/main/java/com/example/assignment/QuestionLibrary.java
@@ -2,17 +2,99 @@ package com.example.assignment;
 
 public class QuestionLibrary {
 
+    private int maxQ = 2;
+
+    //implement array list
     private String mQuestions [] = {
-            "q1",
-            "q2",
+            "what colour is the sky",
+            "what colour is grass",
 
 
     };
 
     private String mAnswers [][] = {
-            {"q1 answer", "q1 answer2", "q1 answer3"},
-            {"q2 answer", "q2 answer2", "q2 answer3"}
+            {"blue", "red", "green", "orange"},
+            {"blue", "black", "green", "purple"},
     };
 
-  // private String mcorrectAnswers[] = "{q1 answer"
+    private String multipleChoiceQuestions[] = {
+            "I am a boy",
+            "I am not a girl"
+    };
+
+    private String multipleChoiceAnswers[] = {"TRUE", "FALSE"};
+
+
+    private String mCorrectAnswers[] = {"blue", "grass"};
+
+
+    public String getQuestion(int a, int b) {
+        if (a == maxQ) {
+
+            String question = multipleChoiceQuestions[b];
+            return question;
+
+
+        } else {
+            String question = mQuestions[a];
+            return question;
+
+        }
+    }
+
+
+
+    public String getChoice1(int a, int b) {
+        if (a!= maxQ) {
+            String choice0 = mAnswers[a][0];
+            return choice0;
+        }
+        else {
+            String choice0 = multipleChoiceAnswers[0];
+            return choice0;
+        }
+    }
+
+
+    public String getChoice2(int a, int b) {
+        if (a != maxQ) {
+
+
+            String choice1 = mAnswers[a][1];
+            return choice1;
+        } else {
+            String choice1 = multipleChoiceAnswers[1];
+            return choice1;
+        }
+    }
+
+
+    public String getChoice3(int a, int b) {
+        if (a != maxQ) {
+            String choice2 = mAnswers[a][2];
+            return choice2;
+        }
+        return null;
+    }
+
+    public String getChoice4(int a, int b) {
+        if (a != maxQ) {
+            String choice2 = mAnswers[a][3];
+            return choice2;
+        }
+        return null;
+    }
+
+    public String getCorrectAnswer(int a, int b) {
+
+        if (a != maxQ){
+        String answer = mCorrectAnswers[a];
+        return answer;}
+        else {
+            String answer = multipleChoiceAnswers[b];
+            return answer;
+        }
+    }
+
 }
+
--- app/src/main/java/com/example/assignment/Quiz.java
+++ app/src/main/java/com/example/assignment/Quiz.java
@@ -1,13 +1,150 @@
 package com.example.assignment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telecom.TelecomManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Quiz extends AppCompatActivity {
+
+    //class to populate and randomize the quiz
+
+    private QuestionLibrary questionLibrary = new QuestionLibrary();
+
+    private int maxQ = 3;
+
+    private int mCQCount = 0;
+
+    private TextView scoreText;
+    private TextView question;
+    private Button choice1;
+    private Button choice2;
+    private Button choice3;
+    private Button choice4;
+
+    private String answer;
+    private int score = 0;
+    private int qNo = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        scoreText = findViewById(R.id.scoreCount);
+        question = findViewById(R.id.questionBox);
+        choice1 = findViewById(R.id.q1);
+        choice2 = findViewById(R.id.q2);
+        choice3 = findViewById(R.id.q3);
+        choice4 = findViewById(R.id.q4);
+
+        updateQ();
+
+        choice1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (choice1.getText() == answer) {
+                    score++;
+                    scoreText.setText("" + score);
+                    Toast.makeText(Quiz.this, "Correct", Toast.LENGTH_SHORT).show();
+                    updateQ();
+                } else {
+                    Toast.makeText(Quiz.this, "Incorrect", Toast.LENGTH_SHORT).show();
+                    updateQ();
+                }
+            }
+        });
+
+        choice2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (choice2.getText() == answer) {
+                    score++;
+                    scoreText.setText("" + score);
+                    Toast.makeText(Quiz.this, "Correct", Toast.LENGTH_SHORT).show();
+                    updateQ();
+                } else {
+                    Toast.makeText(Quiz.this, "Incorrect", Toast.LENGTH_SHORT).show();
+                    updateQ();
+                }
+            }
+        });
+
+        choice3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (choice3.getText() == answer) {
+                    score++;
+                    scoreText.setText("" + score);
+                    Toast.makeText(Quiz.this, "Correct", Toast.LENGTH_SHORT).show();
+                    updateQ();
+                } else {
+                    Toast.makeText(Quiz.this, "Incorrect", Toast.LENGTH_SHORT).show();
+
+                    updateQ();
+                }
+            }
+        });
+
+        choice4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (choice4.getText() == answer) {
+                    score++;
+                    scoreText.setText("" + score);
+                    Toast.makeText(Quiz.this, "Correct", Toast.LENGTH_SHORT).show();
+                    updateQ();
+                } else {
+                    Toast.makeText(Quiz.this, "Incorrect", Toast.LENGTH_SHORT).show();
+                    updateQ();
+                }
+            }
+        });
+
     }
-}
+
+
+        private void updateQ() {
+        if (mCQCount ==2){
+            startActivity(new Intent(Quiz.this,QuizResults.class));
+        }
+            question.setText(questionLibrary.getQuestion(qNo,mCQCount));
+            choice1.setText(questionLibrary.getChoice1(qNo,mCQCount));
+            choice2.setText(questionLibrary.getChoice2(qNo,mCQCount));
+            choice3.setText(questionLibrary.getChoice3(qNo,mCQCount));
+            choice4.setText(questionLibrary.getChoice4(qNo,mCQCount));
+            answer = questionLibrary.getCorrectAnswer(qNo,mCQCount);
+            if (qNo+1 == maxQ) {
+
+             trueFalse();
+             mCQCount++;
+
+            } else  {
+                qNo ++;
+            }
+
+
+
+            }
+
+
+
+    private void trueFalse() {
+
+        choice1.setVisibility(View.GONE);
+        choice2.setVisibility(View.GONE);
+        choice3.setText("TRUE");
+        choice4.setText("FALSE");
+
+
+
+    }
+
+    }
+
+
+
